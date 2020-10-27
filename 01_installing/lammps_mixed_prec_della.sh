229bda95f59cbeef779005364796dd69229ce44c
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-10-26 21:36:40 -0400

    Create lammps_mixed_prec_della.sh

---
+++ 01_installing/lammps_mixed_prec_della.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# build a mixed precision version of lammps for della using the user-intel package
+
+VERSION=4Feb2020
+wget https://github.com/lammps/lammps/archive/patch_${VERSION}.tar.gz
+tar zxf patch_${VERSION}.tar.gz
+cd lammps-patch_${VERSION}
+mkdir build
+cd build
+
+module purge
+module load intel/18.0/64/18.0.3.222 intel-mpi/intel/2018.3/64
+
+# copy and paste the next 5 lines into the terminal
+cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local -D LAMMPS_MACHINE=della -D ENABLE_TESTING=yes \
+-D BUILD_MPI=yes -D BUILD_OMP=yes -D CMAKE_CXX_COMPILER=icpc -D CMAKE_BUILD_TYPE=Release \
+-D CMAKE_CXX_FLAGS_RELEASE="-Ofast -axCORE-AVX512 -DNDEBUG" \
+-D PKG_USER-OMP=yes -D PKG_MOLECULE=yes \
+-D PKG_USER-INTEL=yes -D INTEL_ARCH=cpu -D INTEL_LRT_MODE=threads ../cmake
+
+make -j 10
+make test
+make install
