68f1e66522b04bb75a3ce1846c86eeb1e3b47368
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-02-04 23:04:12 -0500

    Update traverse.sh

--- 01_installing/traverse.sh
+++ 01_installing/traverse.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-version=3Mar2020
+version=29Oct2020
+
+#############################################################
+# you probably don't need to change anything below this line
+#############################################################
 
 wget https://github.com/lammps/lammps/archive/stable_${version}.tar.gz
 tar zxvf stable_${version}.tar.gz
@@ -8,15 +12,12 @@ cd lammps-stable_${version}
 mkdir build
 cd build
 
-# need to edit lammps-stable_3Mar2020/cmake/Modules/Packages/GPU.cmake
-
 module purge
-module load openmpi/gcc/4.0.4/64 cudatoolkit/11.0
+module load openmpi/gcc/4.0.4/64 cudatoolkit/11.2
 
-# copy and paste the next 5 lines into the terminal
 cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local -D CMAKE_BUILD_TYPE=Release -D LAMMPS_MACHINE=traverse \
 -D ENABLE_TESTING=yes -D BUILD_MPI=yes -D BUILD_OMP=yes -D CMAKE_C_COMPILER=gcc \
--D CMAKE_CXX_COMPILER=g++ -D CMAKE_CXX_FLAGS_RELEASE="-Ofast -mcpu=power9 -mtune=power9 -mvsx" \
+-D CMAKE_CXX_COMPILER=g++ -D CMAKE_CXX_FLAGS_RELEASE="-Ofast -mcpu=power9 -mtune=power9" \
 -D PKG_USER-OMP=yes -D PKG_MOLECULE=yes \
 -D PKG_GPU=yes -D GPU_API=cuda -D GPU_PREC=mixed -D GPU_ARCH=sm_70 -D CUDPP_OPT=yes ../cmake
 
