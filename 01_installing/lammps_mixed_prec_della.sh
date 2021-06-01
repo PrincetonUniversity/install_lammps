33e7b4838c585ab572a6f121a650234986843624
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-06-01 11:48:55 -0400

    Update lammps_mixed_prec_della.sh

--- 01_installing/lammps_mixed_prec_della.sh
+++ 01_installing/lammps_mixed_prec_della.sh
@@ -14,6 +14,7 @@ module load intel/18.0/64/18.0.3.222 intel-mpi/intel/2018.3/64
 
 cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local -D LAMMPS_MACHINE=della -D ENABLE_TESTING=yes \
 -D BUILD_MPI=yes -D BUILD_OMP=yes -D CMAKE_CXX_COMPILER=icpc -D CMAKE_BUILD_TYPE=Release \
+-D CMAKE_Fortran_COMPILER=/opt/intel/compilers_and_libraries_2018.3.222/linux/bin/intel64/ifort \
 -D CMAKE_CXX_FLAGS_RELEASE="-Ofast -axCORE-AVX512 -DNDEBUG" \
 -D PKG_USER-OMP=yes -D PKG_MOLECULE=yes -D PKG_RIGID=yes \
 -D PKG_KSPACE=yes -D FFT=MKL -D FFT_SINGLE=yes \
