8f14d35ce4378731f2657889f57dbaa7cff1221f
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-09-16 15:36:50 -0400

    Create lammps_mixed_prec_adroit_gpu_a100.sh

---
+++ 01_installing/lammps_mixed_prec_adroit_gpu_a100.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# build mixed-precision version with user-intel with A100 GPU acceleration
+
+VERSION=29Oct2020
+wget https://github.com/lammps/lammps/archive/stable_${VERSION}.tar.gz
+tar zxf stable_${VERSION}.tar.gz
+cd lammps-stable_${VERSION}
+mkdir build && cd build
+
+# include the modules below in your Slurm scipt
+module purge
+module load intel/19.1.1.217 intel-mpi/intel/2019.7
+module load cudatoolkit/11.4
+
+cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local -D CMAKE_BUILD_TYPE=Release \
+-D LAMMPS_MACHINE=adroitGPU -D ENABLE_TESTING=yes -D BUILD_MPI=yes -D BUILD_OMP=yes \
+-D CMAKE_C_COMPILER=icc -D CMAKE_CXX_COMPILER=icpc \
+-D CMAKE_Fortran_COMPILER=/opt/intel/compilers_and_libraries_2020.1.217/linux/bin/intel64/ifort \
+-D CMAKE_CXX_FLAGS_RELEASE="-Ofast -xHost -DNDEBUG" -D PKG_USER-OMP=yes \
+-D PKG_MOLECULE=yes -D PKG_RIGID=yes \
+-D PKG_KSPACE=yes -D FFT=MKL -D FFT_SINGLE=yes \
+-D PKG_GPU=yes -D GPU_API=cuda -D GPU_PREC=mixed -D GPU_ARCH=sm_80 -D CUDPP_OPT=yes \
+-D PKG_USER-INTEL=yes -D INTEL_ARCH=cpu -D INTEL_LRT_MODE=threads ../cmake
+
+make -j 4
+#make test
+make install
+
+# if you uncomment "make test" you will find:
+# 63% tests passed, 125 tests failed out of 340
+
+# looking at lammps-stable_29Oct2020/build/Testing/Temporary/LastTest.log
+# one can see that the failures seem to arise because the tests do not
+# account for the single precision calculations of user-intel
+
+# one could write to the lammps mailing list to be sure
