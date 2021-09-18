9e320831d1f8068612208844b130e9c285e308f5
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-09-17 20:22:42 -0400

    Create lammps_double_prec_adroit_cpu.sh

---
+++ 01_installing/adroit/lammps_double_prec_adroit_cpu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# double-precision build for multinode CPU jobs
+
+VERSION=31Aug2021
+wget https://github.com/lammps/lammps/archive/refs/tags/patch_${VERSION}.tar.gz
+tar zvxf patch_${VERSION}.tar.gz
+cd lammps-patch_${VERSION}
+mkdir build && cd build
+
+module purge
+module load intel/19.1.1.217 intel-mpi/intel/2019.7
+
+cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local -D LAMMPS_MACHINE=adroit -D ENABLE_TESTING=yes \
+-D BUILD_MPI=yes -D BUILD_OMP=yes -D CMAKE_CXX_COMPILER=icpc -D CMAKE_BUILD_TYPE=Release \
+-D CMAKE_Fortran_COMPILER=/opt/intel/compilers_and_libraries_2020.1.217/linux/bin/intel64/ifort \
+-D CMAKE_CXX_FLAGS_RELEASE="-Ofast -xHost -DNDEBUG" \
+-D PKG_KSPACE=yes -D FFT=MKL -D FFT_SINGLE=no \
+-D PKG_OPENMP=yes -D PKG_MOLECULE=yes -D PKG_RIGID=yes  ../cmake
+
+make -j 10
+#make test
+make install
