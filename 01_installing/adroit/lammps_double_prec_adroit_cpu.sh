7c806d3781038b6cc3477dabca91e618b39188a6
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-09-17 20:32:11 -0400

    Update lammps_double_prec_adroit_cpu.sh

--- 01_installing/adroit/lammps_double_prec_adroit_cpu.sh
+++ 01_installing/adroit/lammps_double_prec_adroit_cpu.sh
@@ -11,12 +11,20 @@ mkdir build && cd build
 module purge
 module load intel/19.1.1.217 intel-mpi/intel/2019.7
 
-cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local -D LAMMPS_MACHINE=adroit -D ENABLE_TESTING=yes \
--D BUILD_MPI=yes -D BUILD_OMP=yes -D CMAKE_CXX_COMPILER=icpc -D CMAKE_BUILD_TYPE=Release \
--D CMAKE_Fortran_COMPILER=/opt/intel/compilers_and_libraries_2020.1.217/linux/bin/intel64/ifort \
+cmake3 \
+-D CMAKE_INSTALL_PREFIX=$HOME/.local \
+-D LAMMPS_MACHINE=adroit \
+-D CMAKE_BUILD_TYPE=Release \
+-D CMAKE_CXX_COMPILER=icpc \
 -D CMAKE_CXX_FLAGS_RELEASE="-Ofast -xHost -DNDEBUG" \
+-D CMAKE_Fortran_COMPILER=/opt/intel/compilers_and_libraries_2020.1.217/linux/bin/intel64/ifort \
+-D BUILD_OMP=yes \
+-D BUILD_MPI=yes \
 -D PKG_KSPACE=yes -D FFT=MKL -D FFT_SINGLE=no \
--D PKG_OPENMP=yes -D PKG_MOLECULE=yes -D PKG_RIGID=yes  ../cmake
+-D PKG_OPENMP=yes \
+-D PKG_MOLECULE=yes \
+-D PKG_RIGID=yes \
+-D ENABLE_TESTING=yes ../cmake
 
 make -j 10
 #make test
