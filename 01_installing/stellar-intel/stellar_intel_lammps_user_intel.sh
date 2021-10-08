1585fe522bb7871ce31fbe02288789ee3287ba7c
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-10-07 23:17:03 -0400

    Update stellar_intel_lammps_user_intel.sh

--- 01_installing/stellar-intel/stellar_intel_lammps_user_intel.sh
+++ 01_installing/stellar-intel/stellar_intel_lammps_user_intel.sh
@@ -7,14 +7,13 @@ cd lammps-stable_${VERSION}
 mkdir build && cd build
 
 module purge
-module load cmake/3.18.2
 module load intel/19.1.1.217
 module load intel-mpi/intel/2019.7
 
-cmake -D CMAKE_INSTALL_PREFIX=$HOME/.local \
+cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
 -D CMAKE_BUILD_TYPE=Release \
 -D LAMMPS_MACHINE=user_intel \
--D ENABLE_TESTING=yes \
+-D ENABLE_TESTING=no \
 -D BUILD_OMP=yes \
 -D BUILD_MPI=yes \
 -D CMAKE_Fortran_COMPILER=/opt/intel/compilers_and_libraries_2020.1.217/linux/bin/intel64/ifort \
@@ -26,12 +25,4 @@ cmake -D CMAKE_INSTALL_PREFIX=$HOME/.local \
 -D PKG_USER-INTEL=yes -D INTEL_ARCH=cpu -D INTEL_LRT_MODE=threads ../cmake
 
 make -j 32
-#make test
 make install
-
-# if you uncomment "make test" you will find:
-# 86% tests passed, 48 tests failed out of 340
-
-# looking at lammps-stable_29Oct2020/build/Testing/Temporary/LastTest.log
-# one can see that the failures can be ignored since they relate to
-# slight differences in the calculated vs. expected values
