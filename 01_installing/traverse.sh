572211dc330029d4a98a2738ee696ae52042e1e6
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-10-07 22:50:53 -0400

    Update traverse.sh

--- 01_installing/traverse.sh
+++ 01_installing/traverse.sh
@@ -3,24 +3,34 @@
 version=29Oct2020
 
 #############################################################
-# you probably don't need to change anything below this line
+# you probably do not need to change anything below this line
 #############################################################
 
 wget https://github.com/lammps/lammps/archive/stable_${version}.tar.gz
 tar zxvf stable_${version}.tar.gz
 cd lammps-stable_${version}
-mkdir build
-cd build
+mkdir build && cd build
 
 module purge
-module load openmpi/gcc/4.0.4/64 cudatoolkit/11.2
+module load openmpi/gcc/4.1.1/64
+module load fftw/gcc/3.3.8
+module load cudatoolkit/11.4
 
-cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local -D CMAKE_BUILD_TYPE=Release -D LAMMPS_MACHINE=traverse \
--D ENABLE_TESTING=yes -D BUILD_MPI=yes -D BUILD_OMP=yes -D CMAKE_C_COMPILER=gcc \
--D CMAKE_CXX_COMPILER=g++ -D CMAKE_CXX_FLAGS_RELEASE="-Ofast -mcpu=power9 -mtune=power9" \
--D PKG_USER-OMP=yes -D PKG_MOLECULE=yes \
+cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
+-D LAMMPS_MACHINE=traverse \
+-D BUILD_MPI=yes \
+-D BUILD_OMP=yes \
+-D CMAKE_BUILD_TYPE=Release \
+-D CMAKE_C_COMPILER=gcc \
+-D CMAKE_CXX_COMPILER=g++ \
+-D CMAKE_CXX_FLAGS_RELEASE="-Ofast -mcpu=power9 -mtune=power9" \
+-D ENABLE_TESTING=no \
+-D PKG_KSPACE=yes -D FFT=FFTW3 -D FFT_SINGLE=no \
+-D FFTW3_INCLUDE_DIR=${FFTW3DIR}/include \
+-D FFTW3_LIBRARY=${FFTW3DIR}/lib64/libfftw3.so \
+-D PKG_MOLECULE=yes \
+-D PKG_RIGID=yes \
 -D PKG_GPU=yes -D GPU_API=cuda -D GPU_PREC=mixed -D GPU_ARCH=sm_70 -D CUDPP_OPT=yes ../cmake
 
 make -j 10
-make test
 make install
