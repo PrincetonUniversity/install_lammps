b5fd48c54bf6f030d6bb775429c795983bdfe2d7
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-10-09 16:19:56 -0400

    Update lammps_mixed_prec_della.sh

--- 01_installing/della/lammps_mixed_prec_della.sh
+++ 01_installing/della/lammps_mixed_prec_della.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-# build a mixed precision version of lammps for della using the user-intel package
+# build a mixed-precision version of lammps for della using the user-intel package
 
-VERSION=4Feb2020
-wget https://github.com/lammps/lammps/archive/patch_${VERSION}.tar.gz
-tar zxf patch_${VERSION}.tar.gz
-cd lammps-patch_${VERSION}
-mkdir build
-cd build
+version=29Sep2021
+wget https://github.com/lammps/lammps/archive/stable_${version}.tar.gz
+tar zxvf stable_${version}.tar.gz
+cd lammps-stable_${version}
+mkdir build && cd build
 
 module purge
-module load intel/18.0/64/18.0.3.222 intel-mpi/intel/2018.3/64
+module load intel/18.0/64/18.0.3.222
+module load intel-mpi/intel/2018.3/64
 
-cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local -D LAMMPS_MACHINE=della -D ENABLE_TESTING=yes \
--D BUILD_MPI=yes -D BUILD_OMP=yes -D CMAKE_CXX_COMPILER=icpc -D CMAKE_BUILD_TYPE=Release \
+cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
+-D LAMMPS_MACHINE=della \
+-D ENABLE_TESTING=yes \
+-D BUILD_MPI=yes \
+-D BUILD_OMP=yes \
+-D CMAKE_CXX_COMPILER=icpc \
+-D CMAKE_BUILD_TYPE=Release \
 -D CMAKE_Fortran_COMPILER=/opt/intel/compilers_and_libraries_2018.3.222/linux/bin/intel64/ifort \
 -D CMAKE_CXX_FLAGS_RELEASE="-Ofast -axCORE-AVX512 -DNDEBUG" \
--D PKG_USER-OMP=yes -D PKG_MOLECULE=yes -D PKG_RIGID=yes \
+-D PKG_MOLECULE=yes \
+-D PKG_RIGID=yes \
 -D PKG_KSPACE=yes -D FFT=MKL -D FFT_SINGLE=yes \
 -D PKG_USER-INTEL=yes -D INTEL_ARCH=cpu -D INTEL_LRT_MODE=threads ../cmake
 
