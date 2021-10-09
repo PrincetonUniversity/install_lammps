ff56597d4864f361b6df1ac4f73c37453849e67e
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-10-09 17:09:22 -0400

    Update della_gpu_lammps_double_gcc.sh

--- 01_installing/della/della_gpu_lammps_double_gcc.sh
+++ 01_installing/della/della_gpu_lammps_double_gcc.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 
-# This script will produce a double precision build which is probably not what you want.
-# Additionally, the performance is not very good.
-# Adding -Ofast or -march=native will cause tests to fail.
-# Do not include the cmake module in your Slurm script.
-# Using FFT_SINGLE=yes with libfftw3f.so will cause tests to fail.
-# There are several issues to work out. Write to cses@princeton.edu if you want to
-# try to achieve optimal performance with LAMMPS on della-gpu.
-
-VERSION=29Oct2020
+VERSION=29Sep2021
 wget https://github.com/lammps/lammps/archive/stable_${VERSION}.tar.gz
 tar zxf stable_${VERSION}.tar.gz
 cd lammps-stable_${VERSION}
 mkdir build && cd build
 
 module purge
-module load cmake/3.18.2
 module load fftw/gcc/3.3.9
 module load openmpi/gcc/4.1.0
 module load cudatoolkit/11.3
 
-cmake -D CMAKE_INSTALL_PREFIX=$HOME/.local \
+cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
 -D CMAKE_BUILD_TYPE=Release \
 -D LAMMPS_MACHINE=della_gpu_gcc \
--D ENABLE_TESTING=yes \
+-D ENABLE_TESTING=no \
 -D BUILD_OMP=yes \
 -D BUILD_MPI=yes \
 -D CMAKE_C_COMPILER=gcc \
 -D CMAKE_CXX_COMPILER=g++ \
--D CMAKE_CXX_FLAGS_RELEASE="-O3 -fopenmp -DNDEBUG" \
+-D CMAKE_CXX_FLAGS_RELEASE="-Ofast -march=native -fopenmp -DNDEBUG" \
 -D CMAKE_Fortran_COMPILER=/usr/bin/gfortran \
 -D PKG_MOLECULE=yes -D PKG_RIGID=yes \
 -D PKG_KSPACE=yes -D FFT=FFTW3 -D FFT_SINGLE=no \
@@ -37,5 +28,4 @@ cmake -D CMAKE_INSTALL_PREFIX=$HOME/.local \
 -D PKG_GPU=yes -D GPU_API=cuda -D GPU_PREC=double -D GPU_ARCH=sm_80 -D CUDPP_OPT=no ../cmake
 
 make -j 16
-make test
 make install
