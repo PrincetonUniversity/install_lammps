3f589bca79631657acd6f5caf7b4c818a9ebcb0d
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-10-09 22:09:45 -0400

    Update stellar_intel_lammps_double.sh

--- 01_installing/stellar/stellar_intel_lammps_double.sh
+++ 01_installing/stellar/stellar_intel_lammps_double.sh
@@ -15,7 +15,7 @@ cd build
 module purge
 module load intel/19.1.1.217 intel-mpi/intel/2019.7
 
-cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local -D LAMMPS_MACHINE=double -D ENABLE_TESTING=yes \
+cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local -D LAMMPS_MACHINE=double -D ENABLE_TESTING=no \
 -D CMAKE_Fortran_COMPILER=/opt/intel/compilers_and_libraries_2020.1.217/linux/bin/intel64/ifort \
 -D BUILD_MPI=yes -D BUILD_OMP=yes -D CMAKE_CXX_COMPILER=icpc -D CMAKE_BUILD_TYPE=Release \
 -D CMAKE_CXX_FLAGS_RELEASE="-Ofast -xHost -DNDEBUG" \
@@ -23,5 +23,4 @@ cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local -D LAMMPS_MACHINE=double -D ENABLE_T
 -D PKG_KSPACE=yes -D FFT=MKL -D FFT_SINGLE=no ../cmake
 
 make -j 10
-make test
 make install
