83c57081e2a274546a1d770cf25942c40fb0bc48
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-10-14 15:40:14 -0400

    Update tigerCpu_user_intel.sh

--- 01_installing/tigercpu/tigerCpu_user_intel.sh
+++ 01_installing/tigercpu/tigerCpu_user_intel.sh
@@ -6,12 +6,19 @@ cd lammps-patch_4Feb2020
 mkdir build && cd build
 
 module purge
-module load intel/18.0/64/18.0.3.222 intel-mpi/intel/2018.3/64
+module load intel/18.0/64/18.0.3.222
+module load intel-mpi/intel/2018.3/64
 
-cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local -D LAMMPS_MACHINE=tigerCpu -D ENABLE_TESTING=yes \
--D BUILD_MPI=yes -D BUILD_OMP=yes -D CMAKE_CXX_COMPILER=icpc -D CMAKE_BUILD_TYPE=Release \
+cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
+-D LAMMPS_MACHINE=tigerCpu \
+-D ENABLE_TESTING=yes \
+-D BUILD_MPI=yes \
+-D BUILD_OMP=yes \
+-D CMAKE_CXX_COMPILER=icpc \
+-D CMAKE_BUILD_TYPE=Release \
 -D CMAKE_CXX_FLAGS_RELEASE="-Ofast -mtune=skylake-avx512 -DNDEBUG" \
--D PKG_USER-OMP=yes -D PKG_MOLECULE=yes -D PKG_RIGID=yes \
+-D PKG_MOLECULE=yes \
+-D PKG_RIGID=yes \
 -D PKG_KSPACE=yes -D FFT=MKL -D FFT_SINGLE=yes \
 -D CMAKE_Fortran_COMPILER=/opt/intel/compilers_and_libraries_2018.3.222/linux/bin/intel64/ifort \
 -D PKG_USER-INTEL=yes -D INTEL_ARCH=cpu -D INTEL_LRT_MODE=threads ../cmake
