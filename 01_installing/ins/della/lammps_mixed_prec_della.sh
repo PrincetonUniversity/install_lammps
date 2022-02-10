5a2c04dfef2b26c418046d92bee529bd2175b137
jdh4 <52128661+jdh4@users.noreply.github.com>
2022-02-10 17:03:27 -0500

    Update lammps_mixed_prec_della.sh

--- 01_installing/ins/della/lammps_mixed_prec_della.sh
+++ 01_installing/ins/della/lammps_mixed_prec_della.sh
@@ -9,8 +9,8 @@ cd lammps-stable_${VERSION}
 mkdir build && cd build
 
 module purge
-module load intel/19.1/64/19.1.1.217
-module load intel-mpi/intel/2019.7/64
+module load intel/19.1.1.217
+module load intel-mpi/intel/2019.7
 
 cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
 -D LAMMPS_MACHINE=della \
