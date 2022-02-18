dfc21c755b3c87a57d961077c884136a278ab046
jdh4 <52128661+jdh4@users.noreply.github.com>
2022-02-18 10:42:32 -0500

    Newer compiler and MPI library to avoid issues with stalled processes

--- 01_installing/ins/stellar/stellar_intel_lammps_user_intel.sh
+++ 01_installing/ins/stellar/stellar_intel_lammps_user_intel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+  
 VERSION=29Sep2021
 wget https://github.com/lammps/lammps/archive/stable_${VERSION}.tar.gz
 tar zxf stable_${VERSION}.tar.gz
@@ -7,8 +7,8 @@ cd lammps-stable_${VERSION}
 mkdir build && cd build
 
 module purge
-module load intel/19.1.1.217
-module load intel-mpi/intel/2019.7
+module load intel/2021.1.2
+module load intel-mpi/intel/2021.3.1
 
 cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
 -D CMAKE_BUILD_TYPE=Release \
@@ -16,8 +16,8 @@ cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
 -D ENABLE_TESTING=no \
 -D BUILD_OMP=yes \
 -D BUILD_MPI=yes \
--D CMAKE_C_COMPILER=icc \
--D CMAKE_CXX_COMPILER=icpc \
+-D CMAKE_C_COMPILER=icx \
+-D CMAKE_CXX_COMPILER=icpx \
 -D CMAKE_CXX_FLAGS_RELEASE="-Ofast -xHost -DNDEBUG" \
 -D PKG_MOLECULE=yes -D PKG_RIGID=yes -D PKG_MISC=yes \
 -D PKG_KSPACE=yes -D FFT=MKL -D FFT_SINGLE=yes \
