29ee9786377415b142a4ed5d0bd9ef8d769df3d7
jdh4 <52128661+jdh4@users.noreply.github.com>
2022-02-23 22:01:44 -0500

    Update stellar_intel_lammps_user_intel.sh

--- 01_installing/ins/stellar/stellar_intel_lammps_user_intel.sh
+++ 01_installing/ins/stellar/stellar_intel_lammps_user_intel.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-VERSION=29Sep2021
+VERSION=29Oct2020
 wget https://github.com/lammps/lammps/archive/stable_${VERSION}.tar.gz
 tar zxf stable_${VERSION}.tar.gz
 cd lammps-stable_${VERSION}
 mkdir build && cd build
 
 module purge
-module load intel/2021.1.2
-module load intel-mpi/intel/2021.3.1
+module load intel/19.1.1.217
+module load intel-mpi/intel/2019.7
 
 cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
 -D CMAKE_BUILD_TYPE=Release \
@@ -16,12 +16,11 @@ cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
 -D ENABLE_TESTING=no \
 -D BUILD_OMP=yes \
 -D BUILD_MPI=yes \
--D CMAKE_C_COMPILER=icx \
--D CMAKE_CXX_COMPILER=icpx \
--D CMAKE_CXX_FLAGS_RELEASE="-Ofast -xHost -DNDEBUG" \
+-D CMAKE_C_COMPILER=icc \
+-D CMAKE_CXX_COMPILER=icpc \
 -D PKG_MOLECULE=yes -D PKG_RIGID=yes -D PKG_MISC=yes \
 -D PKG_KSPACE=yes -D FFT=MKL -D FFT_SINGLE=yes \
--D PKG_INTEL=yes -D INTEL_ARCH=cpu -D INTEL_LRT_MODE=threads ../cmake
+-D PKG_USER-INTEL=yes -D INTEL_ARCH=cpu -D INTEL_LRT_MODE=threads ../cmake
 
 make -j 16
 make install
