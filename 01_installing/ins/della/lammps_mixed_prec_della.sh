8b6e836e200982cb859cd61191d566970ae0780f
jdh4 <52128661+jdh4@users.noreply.github.com>
2023-05-08 10:09:07 -0400

    Update lammps_mixed_prec_della.sh

--- 01_installing/ins/della/lammps_mixed_prec_della.sh
+++ 01_installing/ins/della/lammps_mixed_prec_della.sh
@@ -9,8 +9,8 @@ cd lammps-stable_${VERSION}
 mkdir build && cd build
 
 module purge
-module load intel/19.1.1.217
-module load intel-mpi/intel/2019.7
+module load intel/2022.2.0
+module load intel-mpi/intel/2021.7.0
 
 cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
 -D LAMMPS_MACHINE=della \
@@ -18,8 +18,8 @@ cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
 -D BUILD_MPI=yes \
 -D BUILD_OMP=yes \
 -D CMAKE_BUILD_TYPE=Release \
--D CMAKE_CXX_COMPILER=icpc \
--D CMAKE_CXX_FLAGS_RELEASE="-Ofast -xHost -qopenmp -restrict -DNDEBUG" \
+-D CMAKE_CXX_COMPILER=icpx \
+-D CMAKE_CXX_FLAGS_RELEASE="-Ofast -xHost -qopenmp -DNDEBUG" \
 -D PKG_MOLECULE=yes \
 -D PKG_RIGID=yes \
 -D PKG_KSPACE=yes -D FFT=MKL -D FFT_SINGLE=yes \
