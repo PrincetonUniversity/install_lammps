cf5f8bf1e30f83d0ae5c2a95e4f9df3c097a9ad7
jdh4 <52128661+jdh4@users.noreply.github.com>
2024-04-16 15:47:50 -0400

    Update lammps_mixed_prec_della.sh

--- 01_installing/ins/della/lammps_mixed_prec_della.sh
+++ 01_installing/ins/della/lammps_mixed_prec_della.sh
@@ -2,28 +2,30 @@
 
 # build a mixed-precision version of lammps for della (cpu) using the intel package
 
-VERSION=23Jun2022
-wget https://github.com/lammps/lammps/archive/stable_${VERSION}.tar.gz
-tar zxvf stable_${VERSION}.tar.gz
+VERSION=2Aug2023_update3
+wget https://github.com/lammps/lammps/archive/refs/tags/stable_${VERSION}.tar.gz
+tar zxf stable_${VERSION}.tar.gz
 cd lammps-stable_${VERSION}
 mkdir build && cd build
 
 module purge
-module load intel/19.1.1.217
-module load intel-mpi/intel/2019.7
+module load intel/2024.0
+module load intel-mpi/intel/2021.7.0
+module load intel-mkl/2024.0
 
 cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
--D LAMMPS_MACHINE=della \
+-D CMAKE_BUILD_TYPE=Release \
+-D LAMMPS_MACHINE=intel \
 -D ENABLE_TESTING=no \
 -D BUILD_MPI=yes \
 -D BUILD_OMP=yes \
--D CMAKE_BUILD_TYPE=Release \
--D CMAKE_CXX_COMPILER=icpc \
+-D CMAKE_CXX_COMPILER=icpx \
 -D CMAKE_CXX_FLAGS_RELEASE="-Ofast -xHost -qopenmp -DNDEBUG" \
 -D PKG_MOLECULE=yes \
 -D PKG_RIGID=yes \
+-D PKG_MISC=yes \
 -D PKG_KSPACE=yes -D FFT=MKL -D FFT_SINGLE=yes \
 -D PKG_INTEL=yes -D INTEL_ARCH=cpu -D INTEL_LRT_MODE=threads ../cmake
 
-make -j 10
+make -j 4
 make install
