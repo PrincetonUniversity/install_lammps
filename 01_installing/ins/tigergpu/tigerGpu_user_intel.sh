b419d78aa790d34523bec3926ca630925175b0b9
Jonathan Halverson <52128661+jdh4@users.noreply.github.com>
2024-10-23 23:46:55 -0400

    Update tigerGpu_user_intel.sh

--- 01_installing/ins/tigergpu/tigerGpu_user_intel.sh
+++ 01_installing/ins/tigergpu/tigerGpu_user_intel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=29Sep2021
+VERSION=29Aug2024
 wget https://github.com/lammps/lammps/archive/stable_${VERSION}.tar.gz
 tar zxvf stable_${VERSION}.tar.gz
 cd lammps-stable_${VERSION}
@@ -8,9 +8,10 @@ mkdir build && cd build
 
 # include the modules below in your Slurm script
 module purge
-module load intel/19.1/64/19.1.1.217
-module load intel-mpi/intel/2019.7/64
-module load cudatoolkit/11.3
+module load intel-oneapi/2024.2
+module load intel-mpi/oneapi/2021.13
+module load intel-mkl/2024.2
+module load cudatoolkit/12.6
 
 # add or remove packages if needed
 cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
@@ -19,13 +20,13 @@ cmake3 -D CMAKE_INSTALL_PREFIX=$HOME/.local \
 -D ENABLE_TESTING=no \
 -D BUILD_MPI=yes \
 -D BUILD_OMP=yes \
--D CMAKE_C_COMPILER=icc \
--D CMAKE_CXX_COMPILER=icpc -D CMAKE_CXX_FLAGS_RELEASE="-Ofast -xHost -DNDEBUG" \
+-D CMAKE_CXX_COMPILER=icpx \
+-D CMAKE_CXX_FLAGS_RELEASE="-Ofast -xHost -qopenmp -DNDEBUG" \
 -D PKG_MOLECULE=yes \
 -D PKG_RIGID=yes \
 -D PKG_KSPACE=yes -D FFT=MKL -D FFT_SINGLE=yes \
--D PKG_GPU=yes -D GPU_API=cuda -D GPU_PREC=mixed -D GPU_ARCH=sm_60 -D CUDPP_OPT=no \
+-D PKG_GPU=yes -D GPU_API=cuda -D GPU_PREC=mixed -D GPU_ARCH=sm_90 -D CUDPP_OPT=no \
 -D PKG_INTEL=yes -D INTEL_ARCH=cpu -D INTEL_LRT_MODE=threads ../cmake
 
-make -j 10
+make -j 8
 make install
